1f439fd6e733fe4bab2c207c10e1a6726e2354ab
thelonehegelian <thelonehegelian@gmail.com>
2020-04-27 17:18:29 +0100

    Add pallette component

--- src/Pallette.js
+++ src/Pallette.js
@@ -4,8 +4,12 @@ export default class Pallette extends Component {
 	render () {
 		return (
 			<div>
-				<h1>Pallette</h1>
-				{console.log(this.props)}
+				{/* Navbar */}
+				<div className='Pallette'>
+					{/* Pallette */}
+					<div className='Pallette-colors'>{/* Color boxes */}</div>
+					{/* Footer */}
+				</div>
 			</div>
 		);
 	}
