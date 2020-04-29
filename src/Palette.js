001de090cdf14a9a631065684eb89fc4699af61a
thelonehegelian <thelonehegelian@gmail.com>
2020-04-29 22:49:17 +0100

    Add shade slider

--- src/Palette.js
+++ src/Palette.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
 import './Palette.css';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 export default class Palette extends Component {
+	constructor (props) {
+		super(props);
+		this.state = { level: 500 };
+	}
+
+	changeLevel = (level) => {
+		this.setState({ level });
+		//	console.log(level);
+	};
 	render () {
-		const colorBoxes = this.props.colors.map((c) => (
-			<ColorBox background={c.color} name={c.name} />
+		const colorBoxes = this.props.palette.colors[this.state.level].map((c) => (
+			<ColorBox background={c.hex} name={c.name} />
 		));
 		return (
 			<div>
+				<Slider
+					min={100}
+					max={900}
+					step={100}
+					defaultValue={this.state.level}
+					onAfterChange={this.changeLevel}
+				/>
 				{/* Navbar */}
 				<div className='Palette'>
 					{/* Palette */}
